6d631740806085fb2bbeb3ec5c1bac73d49f0d79
Dwi Sam <dwisamsiarto@gmail.com>
2022-05-26 23:03:24 +0700

    Hero Commit

--- src/parts/IconText.js
+++ src/parts/IconText.js
@@ -5,7 +5,7 @@ import Button from "elements/Button";
 export default function IconText() {
   return (
     <Button className="brand-text-icon" href="" type="link">
-      Libur<span className="text-gray-900">In.</span>
+      libur<span className="text-gray-900">In.</span>
     </Button>
   );
 }
